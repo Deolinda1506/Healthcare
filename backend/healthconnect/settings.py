1dc610b05656825ad50c2d55a260403ca0f3edc6
Deolinda1506 <g.bogore@alustudent.com>
2024-12-05 16:56:45 +0200

    Prepare for Render deployment

--- backend/healthconnect/settings.py
+++ backend/healthconnect/settings.py
@@ -23,9 +23,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ce)bd$^5zdt26)u3v^kf@%*b3lk%+94a82l%y@j^fvv&xb#q9r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # ... other middleware
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Application definition
